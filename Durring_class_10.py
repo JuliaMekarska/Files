8df44f7c767e2763c1968f91999ba87174c5208d
JuliaMekarska <92386630+JuliaMekarska@users.noreply.github.com>
2021-11-17 16:32:13 +0100

    Add files via upload

---
+++ Durring_class_10.py
@@ -0,0 +1,10 @@
+file = open('Personal_data.txt', 'w')
+
+print("""Julia
+Mękarska
+Uniwersytet Ekonomiczny w Krakowie
+Informatyka""")
+file.close()
+
+file = file = open('Personal_data.txt', 'r')
+
